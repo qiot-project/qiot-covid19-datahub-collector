499a4893d99e782ea391a0f66f0fd29132fc394a
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-04-17 19:09:26 +0200

    Prepare 1.0.0

--- buildpollution.sh
+++ buildpollution.sh
@@ -1,8 +1,8 @@
 chmod +x mvn*
 mvn -N io.takari:maven:wrapper
 ./mvnw clean package -Pnative -pl qiot-datahub-collector-pollution -am -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
-docker rmi quay.io/qiotcovid19/qiot-datahub-collector-pollution:1.0.0-alpha
+docker rmi quay.io/qiotcovid19/qiot-datahub-collector-pollution:1.0.0
 cd qiot-datahub-collector-pollution
-docker build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/qiot-datahub-collector-pollution:1.0.0-alpha .
+docker build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/qiot-datahub-collector-pollution:1.0.0 .
 cd ..
-docker push quay.io/qiotcovid19/qiot-datahub-collector-pollution:1.0.0-alpha
+docker push quay.io/qiotcovid19/qiot-datahub-collector-pollution:1.0.0
